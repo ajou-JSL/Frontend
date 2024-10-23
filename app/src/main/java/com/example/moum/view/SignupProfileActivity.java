a66a777dbf93bfd380ff54e8e45552bd9aecd164
sosongha3 <sosongha3@ajou.ac.kr>
2024-10-24 00:07:15 +0900

    💄 DatePicker로 이력 추가

--- app/src/main/java/com/example/moum/view/SignupProfileActivity.java
+++ app/src/main/java/com/example/moum/view/SignupProfileActivity.java
@@ -1,17 +1,24 @@
 package com.example.moum.view;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.PickVisualMediaRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -19,6 +26,8 @@ import com.bumptech.glide.Glide;
 import com.example.moum.R;
 import com.example.moum.databinding.ActivitySignupProfileBinding;
 import com.example.moum.viewmodel.SignupViewModel;
+
+import java.util.Calendar;
 
 public class SignupProfileActivity extends AppCompatActivity {
 
@@ -35,6 +44,7 @@ public class SignupProfileActivity extends AppCompatActivity {
         signupViewModel = new ViewModelProvider(this).get(SignupViewModel.class);
         View view = binding.getRoot();
         setContentView(view);
+        context = this;
 
         /*Photo picker 선택 후 콜백*/
         ActivityResultLauncher<PickVisualMediaRequest> pickMedia = registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
@@ -84,10 +94,7 @@ public class SignupProfileActivity extends AppCompatActivity {
         });
 
         /*address 스피너 Adapter 연결*/
-        Spinner addressSpinner = binding.spinnerProficiency;
-        /**
-         * TO-DO: address list를 주소 API에서 끌어오기
-         */
+        Spinner addressSpinner = binding.spinnerAddress;
         String[] addressList = getResources().getStringArray(R.array.address_list);
         ArrayAdapter<String> addressAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, addressList);
         addressAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -107,15 +114,71 @@ public class SignupProfileActivity extends AppCompatActivity {
         });
 
 
+        /*이력 레이아웃 동적 생성*/
+        final Calendar calendar = Calendar.getInstance();
+        int thisYear = calendar.get(Calendar.YEAR);
+        int thisMonth = calendar.get(Calendar.MONTH);
+        int thisDay = calendar.get(Calendar.DAY_OF_MONTH);
+        LinearLayout recordParent = binding.signupRecordParent;
+        binding.buttonAddRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // inflate
+                View recordChild = LayoutInflater.from(SignupProfileActivity.this).inflate(R.layout.signup_record_form, recordParent, false);
+
+                // child 세팅
+                LinearLayout placeholderRecordName = recordChild.findViewById(R.id.signup_placeholder_record_name);
+                EditText edittextRecordName = recordChild.findViewById(R.id.signup_edittext_record_name);
+                AppCompatButton buttonRecordStart = recordChild.findViewById(R.id.button_record_date_start);
+                AppCompatButton buttonRecordEnd = recordChild.findViewById(R.id.button_record_date_end);
+                edittextRecordName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                    @Override
+                    public void onFocusChange(View view, boolean hasFocus) {
+                        if(hasFocus){
+                            placeholderRecordName.setBackground(ContextCompat.getDrawable(context, R.drawable.background_rounded_mint_stroke));
+                        }else{
+                            placeholderRecordName.setBackground(ContextCompat.getDrawable(context, R.drawable.background_rounded_gray_stroke));
+                        }
+                    }
+                });
+                buttonRecordStart.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        DatePickerDialog datePickerDialog = new DatePickerDialog(SignupProfileActivity.this,
+                                (mView, mYear, mMonth, mDay) -> {
+                                    String selectedDate = mYear + "년 " + (mMonth + 1) + "월 " + mDay + "일";
+                                    buttonRecordStart.setText(selectedDate);
+                                }, thisYear, thisMonth, thisDay);
+                        datePickerDialog.show();
+                    }
+                });
+                buttonRecordEnd.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        DatePickerDialog datePickerDialog = new DatePickerDialog(SignupProfileActivity.this,
+                                (mView, mYear, mMonth, mDay) -> {
+                                    String selectedDate = mYear + "년 " + (mMonth + 1) + "월 " + mDay + "일";
+                                    buttonRecordEnd.setText(selectedDate);
+                                }, thisYear, thisMonth, thisDay);
+                        datePickerDialog.show();
+                    }
+                });
+
+                // parent에 child view 추가
+                recordParent.addView(recordChild);
+            }
+        });
+
         /*각 placeholder 포커스 시 이벤트*/
         binding.signupEdittextNickname.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View view, boolean hasFocus) {
                 if(hasFocus){
                     binding.signupErrorNickname.setText("");
-                    binding.placeholderNickname.setBackground(ContextCompat.getDrawable(context, R.drawable.background_rounded_mint_stroke));
+                    binding.signupPlaceholderNickname.setBackground(ContextCompat.getDrawable(context, R.drawable.background_rounded_mint_stroke));
                 }else{
-                    binding.placeholderNickname.setBackground(ContextCompat.getDrawable(context, R.drawable.background_rounded_gray_stroke));
+                    binding.signupPlaceholderNickname.setBackground(ContextCompat.getDrawable(context, R.drawable.background_rounded_gray_stroke));
                 }
             }
         });
@@ -124,9 +187,9 @@ public class SignupProfileActivity extends AppCompatActivity {
             public void onFocusChange(View view, boolean hasFocus) {
                 if(hasFocus){
                     binding.signupErrorProfileDescription.setText("");
-                    binding.placeholderProfileDescription.setBackground(ContextCompat.getDrawable(context, R.drawable.background_rounded_mint_stroke));
+                    binding.signupPlaceholderProfileDescription.setBackground(ContextCompat.getDrawable(context, R.drawable.background_rounded_mint_stroke));
                 }else{
-                    binding.placeholderProfileDescription.setBackground(ContextCompat.getDrawable(context, R.drawable.background_rounded_gray_stroke));
+                    binding.signupPlaceholderProfileDescription.setBackground(ContextCompat.getDrawable(context, R.drawable.background_rounded_gray_stroke));
                 }
             }
         });
@@ -135,9 +198,9 @@ public class SignupProfileActivity extends AppCompatActivity {
             public void onFocusChange(View view, boolean hasFocus) {
                 if(hasFocus){
                     binding.signupErrorInstrument.setText("");
-                    binding.placeholderInstrument.setBackground(ContextCompat.getDrawable(context, R.drawable.background_rounded_mint_stroke));
+                    binding.signupPlaceholderInstrument.setBackground(ContextCompat.getDrawable(context, R.drawable.background_rounded_mint_stroke));
                 }else{
-                    binding.placeholderInstrument.setBackground(ContextCompat.getDrawable(context, R.drawable.background_rounded_gray_stroke));
+                    binding.signupPlaceholderInstrument.setBackground(ContextCompat.getDrawable(context, R.drawable.background_rounded_gray_stroke));
                 }
             }
         });
