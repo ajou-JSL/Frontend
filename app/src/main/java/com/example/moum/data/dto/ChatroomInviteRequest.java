e1254034f6fcf718f873a8b11fbc26536a9d0d8b
sosongha3 <sosongha3@ajou.ac.kr>
2024-12-07 01:52:52 +0900

    🐛 채팅방 초대 service 필드명 수정

--- app/src/main/java/com/example/moum/data/dto/ChatroomInviteRequest.java
+++ app/src/main/java/com/example/moum/data/dto/ChatroomInviteRequest.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 public class ChatroomInviteRequest {
     private Integer type;
-    private ArrayList<Integer> members;
+    private ArrayList<Integer> memberIds;
 
-    public ChatroomInviteRequest(Integer type, ArrayList<Integer> members) {
+    public ChatroomInviteRequest(Integer type, ArrayList<Integer> memberIds) {
         this.type = type;
-        this.members = members;
+        this.memberIds = memberIds;
     }
 }
